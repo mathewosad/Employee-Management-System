e66b52b2b874b0012a9c4333efa3b15a7357fb00
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-02-20 17:48:48 -0800

    added comments and requirements

---
+++ app.js
@@ -0,0 +1,100 @@
+//requirements
+// Build a command-line application that at a minimum allows the user to:
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
+
+// Bonus points if you're able to:
+// Update employee managers
+// View employees by manager
+// Delete departments, roles, and employees
+// View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+
+//================================================
+
+//dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
+
+//figlet application name
+figlet("Employee \n \n Manager", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
+
+//create the connection for database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employee_management_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connection established!');
+    start();
+});
+
+// Add departments, roles, employees
+// View departments, roles, employees
+// Update employee roles
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update employee role",
+            "Exit"
+        ]
+    }).then((answer) => {
+        switch (answer.action) {
+            case "View all departments":
+                viewDepts();
+                break;
+
+            case "View all roles":
+                viewRoles();
+                break;
+
+            case "View all employees":
+                viewEes();
+                break;
+
+            case "Add a department":
+                addDept();
+                break;
+
+            case "Add a role":
+                addRole();
+                break;
+
+            case "Add an employee":
+                addEe();
+                break;
+
+            case "Update employee role":
+                update();
+                break;
+
+            case "Exit":
+                connection.end();
+                break;
+        }
+    });
+}
+
+//===================functions=====================
+
+// function to View all departments,
+function viewDepts() {
+    connection.query("SELECT * FROM department", (err, data) => {
