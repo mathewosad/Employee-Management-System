3d1e0719d46f16afea849187e69413fa0b8fce16
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-03-28 22:38:08 -0700

    adding files

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,20 +10,20 @@ VALUES
 
 INSERT INTO role(title, salary, department_id)
 VALUES
-    ("Sales Lead", 100000, 1),
+    ("Admin", 100000, 1),
     ("Salesperson", 80000, 1),
-    ("Lead Engineer", 150000, 2),
+    ("mechanical Engineer", 150000, 2),
     ("Software Engineer", 120000, 2),
-    ("Accountant", 120000, 3),
-    ("Legal Team Lead", 250000, 4),
+    ("Finacial advisor", 120000, 3),
+    ("Legal Aid", 250000, 4),
     ("Lawyer", 190000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-    ("John", "Doe", 1, NULL),
-    ("Mike", "Chan", 2, 1),
-    ("Ashley", "Rodriguez", 3, NULL),
-    ("Kevin", "Tupik", 4, 3),
-    ("Malia", "Brown", 5, NULL),
-    ("Sarah", "Lourd", 6, NULL),
-    ("Tammer", "Galal", 7, 6);
+    ("Jane", "Doe", 1, NULL),
+    ("Micheal", "Spicer", 2, 1),
+    ("Fenado", "Escobar", 3, NULL),
+    ("Calvin", "Silk", 4, 3),
+    ("Madona", "Red", 5, NULL),
+    ("Sharon", "Stone", 6, NULL),
+    ("Terry", "Luise", 7, 6);
